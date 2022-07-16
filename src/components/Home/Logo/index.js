aa94ba145423b100c7930599fc28f347c2fd7e2b
Gebrielsam <samgb042@gmail.com>
2022-07-16 10:08:59 +0100

    third commit

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
-import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
+import { TextPlugin } from 'gsap/TextPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
@@ -10,7 +10,7 @@ const Logo = () => {
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    gsap.registerPlugin(TextPlugin)
 
     gsap
       .timeline()
